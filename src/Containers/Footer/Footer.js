5fe88b28216fcf887529e2f0d731dd633153ad66
vipuly1 <56862842+vipuly1@users.noreply.github.com>
2021-10-31 21:23:18 +0530

    Update Footer.js

--- src/Containers/Footer/Footer.js
+++ src/Containers/Footer/Footer.js
@@ -6,13 +6,9 @@ class Footer extends Component {
       <footer className="tm-footer row tm-mt-small">
         <div className="col-12 font-weight-light">
           <p className="text-center text-white mb-0 px-6 large">
-            Copyright © <b>2021</b> All rights reserved. Design:{" "}
-            <a
-              rel="nofollow noopener"
-              href="https://templatemo.com"
-              className="tm-footer-link"
-            >
-              Template Mo <br/>By Akash Magar
+            Copyright © <b>2021</b> All rights reserved.
+           
+
             </a>
           </p>
         </div>
